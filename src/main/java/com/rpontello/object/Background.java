1153fb50c1c5b70ddd7d0d6f7660f3fb6e362e76
Rafael Pontello <rpontello@outlook.com>
2026-09-21 21:47:51 +0100

    trying to create a tile manager

--- src/main/java/com/rpontello/object/Background.java
+++ src/main/java/com/rpontello/object/Background.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class Background implements GameObject {
 
-    private Sprite sprite;
+    private final Sprite sprite;
 
     public Background() {
         this.sprite = new Sprite("/assets/bg_blue.png");
@@ -23,23 +23,6 @@ public class Background implements GameObject {
     public void render(Graphics2D g) {
 
 
-        Rectangle area = new Rectangle(0, 0, 64, 64);
-        TexturePaint texture = new TexturePaint(sprite.getBuffer(), area);
-
-        g.setPaint(texture);
-        g.fillRect(0, 0, 800, 600);
-
-
-        var x = 0;
-        while(x < Game.GAME_WIDTH) {
-            g.drawImage(TileManager.get(7), x, 500, 16, 16, null);
-            g.drawImage(TileManager.get(9), x, 516, 16, 16, null);
-            g.drawImage(TileManager.get(9), x, 532, 16, 16, null);
-            g.drawImage(TileManager.get(9), x, 548, 16, 16, null);
-            g.drawImage(TileManager.get(9), x, 564, 16, 16, null);
-
-            x += 16;
-        }
-
     }
+
 }
